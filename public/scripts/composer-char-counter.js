35cc16d7b8628c88472513b600450bb89053440b
Turner Schumann <turnerschumann@gmail.com>
2019-05-14 21:50:07 +0000

    CSS styling and adding example profile picture

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   $(".new-tweet textarea").on('keypress', function() {
     charactersEntered = $("textarea").val().length
     remaining = 139 - charactersEntered
-    counter = $(this).siblings('.counter');
+    let counter = $(this).siblings('.counter');
     console.log(counter);
 
     if (remaining < 0) {
